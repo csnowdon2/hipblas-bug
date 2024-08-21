6f5e0b4a0e1ae5d8d4c89eeca16382532b8a0f33
Calum Snowdon <calumsnowdon549@gmail.com>
2024-08-21 09:51:49 +0800

    Added abs to calculation in check_matrices.py

--- check_matrices.py
+++ check_matrices.py
@@ -4,5 +4,5 @@ A = pd.read_csv("A.csv", header=None)
 B = pd.read_csv("B.csv", header=None)
 C = pd.read_csv("C.csv", header=None)
 
-diff = C - (A @ B.T)
+diff = abs(C - (A @ B.T))
 print("Delta", diff.max().max())
